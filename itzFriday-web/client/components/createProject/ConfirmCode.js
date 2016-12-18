2c53759563102ef5a3ad1dd3cc21dc27cd52297b
suganya-g <suganyagopal94@gmail.com>
2016-12-18 12:30:17 +0530

    modified the alignment

--- itzFriday-web/client/components/createProject/ConfirmCode.js
+++ itzFriday-web/client/components/createProject/ConfirmCode.js
@@ -2,7 +2,8 @@ import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import Formsy from 'formsy-react';
 import FormsyText from 'formsy-material-ui/lib/FormsyText';
-import Paper from 'material-ui/Paper'
+import Paper from 'material-ui/Paper';
+import NotificationPriorityHigh from 'material-ui/svg-icons/notification/priority-high';
 import {Grid, Row, Col} from 'react-flexbox-grid';
 import {Link} from 'react-router';
 
@@ -12,10 +13,13 @@ const errorMessages = {
 }
 const styles={
      paperStyle: {
-        width: '100%',
-        margin: 'auto',
-        padding: '10px',
-        height: window.innerHeight
+        position:'absolute',
+        top:'20%',
+        right:'20%',
+        left:'20%',
+        bottom:'20%',
+        width:'60%',
+        height:'60%'
       }
 }
 
@@ -49,12 +53,17 @@ constructor(props){
 
 render(){
     return(
-      
+
       <Grid>
       <Paper style={styles.paperStyle}>
       <Row center="xs">
       <Col xs={12} sm={12} md={12} lg={12}>
-      <h3 style={{color:'#607D8B'}}>Please enter the confirmation code sent to you via email</h3>
+      <br/>
+      <div style={{margin:'auto'}}>
+      <NotificationPriorityHigh style={{color:'#004D40',width:'50px',height:'50px'}} />
+      <h2 style={{color:'#004D40'}}>Verify it&#39;s you!</h2>
+      <h5 style={{color:'#607D8B'}}>Just to make sure it&#39;s really you, we&#39;ve sent you an email with a passcode to enter.</h5>
+    <strong><h4 style={{color:'#004D40'}}>Don&#39;t worry, it&#39;s super easy!</h4></strong>
         <Formsy.Form
             onValid={this.enableButton}
             onInvalid={this.disableButton}
@@ -64,21 +73,23 @@ render(){
               validations="isNumeric,maxLength:6,minLength:6"
               validationError={errorMessages.numberError}
               required
-              floatingLabelText="Confirmation code"
+              floatingLabelText="Enter your confirmation code"
               updateImmediately/><br />
-            <RaisedButton 
+            <RaisedButton
                 type="submit"
                 label="Continue"
                 primary={true}
                 backgroundColor="#4CAF50"
-                disabled={!this.state.canSubmit}/>       
+                disabled={!this.state.canSubmit}/>
+          <strong><h4 style={{color:'#004D40'}}>Thanks for helping us keep ItzFriday secure!</h4></strong>
         </Formsy.Form>
+        </div>
         </Col>
         </Row>
         </Paper>
         </Grid>
-      
+
           );
-           
+
   }
 }
