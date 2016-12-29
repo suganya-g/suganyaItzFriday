fc56e5bbd0ac38a494999e2a3d316eb30b6eab71
Ruchika <ruchikasaklani07@gmail.com>
2016-12-29 11:51:09 +0530

    chatinput

--- itzFriday-web/client/components/chats/ChatInput.js
+++ itzFriday-web/client/components/chats/ChatInput.js
@@ -6,48 +6,63 @@ import FormsyText from 'formsy-material-ui/lib/FormsyText';
 import IconButton from 'material-ui/IconButton';
 import ContentSend from 'material-ui/svg-icons/content/send';
 import Auth from './../../services/auth.service.js'
+import SvgIcon from 'material-ui/SvgIcon'
+import {Picker} from 'emoji-mart';
+import {Emoji} from 'emoji-mart';
+import Dialog from 'material-ui/Dialog';
 
 const styles = {
-	inputArea: {
-		width: "100%"
-	}
+  inputArea: {
+    width: "100%"
+  }
 }
 class ChatInput extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			canSubmit: false,
-		}
-	}
-	enableButton() {
-    	this.setState({
-      		canSubmit: true
-    	});
-  	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      canSubmit: false,
+      emo: false
+    }
+  }
+  enableButton() {
+      this.setState({
+          canSubmit: true
+      });
+    }
 
-  	disableButton() {
-    	this.setState({
-      	canSubmit: false
-    	});
-  	}
+    disableButton () {
+      this.setState({
+        canSubmit: false
+      });
+    }
 
-  	submitForm(data) {
+   showEmojiPicker = () => {
+      this.setState({emo: !this.state.emo});
+    }
+
+    handleEmoji = (emoji) => {
+      let emojiString = '<Emoji emoji=":'+ emoji['id'] + ':" size={24} />'
+      console.log(emojiString);
+    }
+
+
+    submitForm(data) {
       var localTime  = moment.utc(new Date()).toDate();
-    	var newMessage = {
-      		chatTime: moment(localTime).format('llll'),
-      		chatText: data.messages,
-      		authorAvtar: "https://twitter.com/@"+this.props.userName+"/profile_image?size=original"
-    	}
-    	this.props.addChat(newMessage);
-		  this.refs.chat.setState({value: ''});
-  	}                                        
-  	notifyFormError(data) {
-    	console.error('Form error:', data);
-  	}
+      var newMessage = {
+          chatTime: moment(localTime).format('llll'),
+          chatText: data.messages,
+          authorAvtar: "https://twitter.com/@"+this.props.userName+"/profile_image?size=original"
+      }
+      this.props.addChat(newMessage);
+      this.refs.chat.setState({value: ''});
+    }                                        
+    notifyFormError(data) {
+      console.error('Form error:', data);
+    }
 
-	render() {
-		return(
-			<Formsy.Form
+  render() {
+    return(
+      <Formsy.Form
                     onValid={this.enableButton.bind(this)}
                     onInvalid={this.disableButton.bind(this)}
                     onValidSubmit={this.submitForm.bind(this)}
@@ -70,20 +85,38 @@ class ChatInput extends Component {
                   style = {styles.inputArea}
                 />
                 </Col>
-                <Col xs={2} sm={2} md={2} lg={2}>
+                      <Emoji
+                    emoji=':blush:'
+                    size={24}
+                    onClick = {this.showEmojiPicker}
+                  />                 
+              
+                    {this.state.emo?
+                      <Picker
+                      style={{marginBottom:'20px'}}
+                      onClick={ this.handleEmoji }
+                      emojiSize={20}
+                      perLine={7}
+                      skin={1}
+                      set='apple'
+                      />
+                      :
+                      <span/>
+                    }
+              
                   <IconButton
                     type="submit"
-                    disabled={!this.state.canSubmit}
-                  ><ContentSend/></IconButton>
-                </Col>
-                </Row>
-                </Col>
-              </Row>
-            </Grid>
-            </Formsy.Form>
-			)
-	}
+                    ><ContentSend/></IconButton>
+                    </Row>
+                    </Col>
+                    </Row>
+                    </Grid>
+                    </Formsy.Form>
+                    )
+        }
 
-}
+      }
 
-export default ChatInput;
+      export default ChatInput;
+
+
