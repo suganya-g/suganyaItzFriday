56dba5bbb2fcc907f77adcc0d0ee980e277d4751
suganya-g <suganyagopal94@gmail.com>
2017-01-03 18:02:37 +0530

    added the html part

--- itzFriday-web/server/service/sendmail.js
+++ itzFriday-web/server/service/sendmail.js
@@ -13,8 +13,8 @@ var transporter = nodemailer.createTransport({
     from: 'itzzfriday@gmail.com', // sender address
     to: mailId, // list of receivers
     subject: 'Confirmation Code', // Subject line
-    text: "Please enter the given confirmation code "+code //, // plaintext body
-    // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
+    // text: "Please enter the given confirmation code "+code //, // plaintext body
+    html: '<div style="border:2px solid black;width:1000px;height:600px;text-align:center">'+'<img src="http://www.clipartkid.com/images/76/original-free-christian-clip-art-little-green-frog-1-so-happy-to-wdRepB-clipart.gif" style="text-align:center" width="300px" height="300px" />'+'<h4>Hello</h4>'+'<h4>Thank you for visiting</h4>'+'<h1 style="color:green">ItzFriday!</h1>'+'<h4>To help you get started with your team confirm your email address by entering the below code</h4><br />'+'<div style="padding-left:430px"><h1 style="border:1px solid black;width:150px;height:30px;text-align:center">'+code+'</h1></div></div>' // You can choose to send an HTML body instead
 };
 
 transporter.sendMail(mailOptions, function(error, info){
